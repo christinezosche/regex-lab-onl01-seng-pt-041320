687858c815b4f6caee686a64f4fb54a9b04fc104
Christine Zosche <christine.zosche@gmail.com>
2020-04-28 20:17:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/\b[A-Z].*+[,.?;:'"!]\b/)
+  if text.scan(/\b[A-Z].*+[,.?;:'"!]\z/)
     return true
   else
     return false
